4a3c13428b3426b2b26b69071d1ddcefff40acbd
Ellis Marshall <ellis.marshall215@gmail.com>
2023-02-10 15:50:00 -0500

    psudo code

---
+++ main.js
@@ -0,0 +1,16 @@
+function getComputerChoice() {
+    //choose random computer selection
+}
+
+function getPlayerChoice() {
+    //get player input prompt(message)
+}
+
+function playRound (playerChoice,ComputerChoice){
+    //if statemant to determine winner
+    //string announces winner
+}
+
+function playGame() {
+    //play 5 rounds call playRound() function
+}
